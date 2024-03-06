f8103d3d5af5f1c609527797f29d5ec86ff4c6c1
Arun Kumar <aruncode49@gmail.com>
2024-03-06 16:33:47 +0530

    arrays and tuples in typescript

---
+++ Lession-2/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Arrays and Objects
+let stringArr = ["a", "b", "c"]; // string[]
+let numAndStringArr = [1, "a", "b"]; // (number | string)[]
+let mixedArr = [1, "a", true]; // (number | string | boolean)[]
+let test = []; // any[]
+let bands = [];
+// Tuple (for more strictness) => for specific length and position for their desired data types
+let myTuple = ["hello", 12, true];
---
+++ Lession-2/src/main.ts
@@ -0,0 +1,11 @@
+// Arrays and Objects
+
+let stringArr = ["a", "b", "c"]; // string[]
+let numAndStringArr = [1, "a", "b"]; // (number | string)[]
+let mixedArr = [1, "a", true]; // (number | string | boolean)[]
+
+let test = []; // any[]
+let bands: string[] = [];
+
+// Tuple (for more strictness) => for specific length and position for their desired data types
+let myTuple: [string, number, boolean] = ["hello", 12, true];
