6220d51f72329902b70e4f1785759e6f38355435
Arun Kumar <aruncode49@gmail.com>
2024-03-08 15:27:13 +0530

    functions in typescript

--- 3 Functions/index.ts
+++ 3 Functions/index.ts
@@ -26,3 +26,25 @@ function loginUser(name: string, email: string, isAdmin?: string) {
 }
 
 loginUser("abc", "abc@gmail.com");
+
+// Some better practice to use functions in typescript
+
+function consoleErrors(errMsg: string): void {
+  console.log(errMsg);
+}
+
+const fruits = ["apple", "banana", "grapes", "oranges"];
+
+// we want to return string[] only from map fn.
+fruits.map((fruit, index): string => {
+  return `${index + 1} :: ${fruit}`;
+});
+
+// never -> type
+// some functions never return a value :: similar to void (but different)
+
+function handleError(errMsg: string): never {
+  throw new Error(errMsg); // intentionaly crashed our project
+
+  // ..... sometimes stuck into infinite loop
+}
