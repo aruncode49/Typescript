069c94ba50d4a338570de3a456f067ce17a7ae3b
Arun Kumar <aruncode49@gmail.com>
2024-03-08 17:43:05 +0530

    read only and optional in typescript

---
+++ 5 Type Alias/readOnly_optional.ts
@@ -0,0 +1,40 @@
+// readonly & optional
+
+type User = {
+  readonly _id: string; // nobody can touch this _id
+  name: string;
+  email: string;
+  isAdmin: boolean;
+  creditCardNumber?: number; // this field is optional, if we don't provide creditCardNumber typescript will not give any error
+};
+
+let myUser: User = {
+  _id: "123456",
+  name: "",
+  email: "",
+  isAdmin: false,
+};
+
+myUser.name = "abc";
+myUser.email = "abc@gmail.com";
+// myUser._id = "abc@123"  // error because of read only property
+
+// Qs: If id is an array and we applied readonly then on push method, it gives error or not?
+
+type testObj = {
+  readonly id: string[];
+};
+
+let testId: testObj = { id: ["a", "b", "c"] };
+
+// now these are mutable and no error occured!!
+testId.id.push("abc");
+testId.id.pop();
+testId.id.pop();
+
+// realy usecase of type alias (develop new type from previous types)
+type CardNumber = number;
+type CardDate = string;
+
+// developing new type from previous types
+type CardDetails = CardDate | CardNumber | { cvv: number };
