17a482dca754f674e743065cd45e95e20c89c387
Arun Kumar <aruncode49@gmail.com>
2024-03-11 17:10:45 +0530

    public, private, getter and setter

--- 11-Classes and Objects/dist/main.js
+++ 11-Classes and Objects/dist/main.js
@@ -1,11 +1,37 @@
 "use strict";
-/************** Class in Typescript **********************/
+/************** Classes in Typescript **********************/
+// class User {
+//   public email: string;
+//   private name: string; // only accessible with in the class (# in js for private eg: #name)
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// other way
 class User {
-    constructor(email, name) {
-        this.city = "";
+    constructor(email, name // public userId: string
+    ) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1; // default value, can't access and manipulate outside the class, we need getter and setter for that!
+        this.city = "";
+    }
+    get getGoogleEmail() {
+        return `google${this.email},`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        this._courseCount = courseNum;
     }
 }
 const arun = new User("arun@aj.com", "arun");
-// arun.city = "Rohtak";  => not allowed
+const city = arun.city; // we can access it but can't modify it
+// getter
+const courseCount = arun.courseCount;
+// setter
+arun.courseCount = 5;
+// NOTE :: no open parenthesis required for using getter and setter
--- 11-Classes and Objects/src/main.ts
+++ 11-Classes and Objects/src/main.ts
@@ -1,14 +1,45 @@
-/************** Class in Typescript **********************/
+/************** Classes in Typescript **********************/
 
+// class User {
+//   public email: string;
+//   private name: string; // only accessible with in the class (# in js for private eg: #name)
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+// other way
 class User {
-  email: string;
-  name: string;
+  private _courseCount = 1; // default value, can't access and manipulate outside the class, we need getter and setter for that!
+
   readonly city: string = "";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
+  constructor(
+    public email: string,
+    public name: string // public userId: string
+  ) {}
+
+  get getGoogleEmail(): string {
+    return `google${this.email},`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    this._courseCount = courseNum;
   }
 }
 
 const arun = new User("arun@aj.com", "arun");
-// arun.city = "Rohtak";  => not allowed
+const city = arun.city; // we can access it but can't modify it
+
+// getter
+const courseCount = arun.courseCount;
+
+// setter
+arun.courseCount = 5;
+
+// NOTE :: no open parenthesis required for using getter and setter
