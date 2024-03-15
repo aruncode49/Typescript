b78ae75262772a22b580a7493475a8421504786a
Arun Kumar <aruncode49@gmail.com>
2024-03-15 15:20:05 +0530

    another usefull refactor

--- dist/tuples.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Tuple => fix position of types and fix length
-const user = ["arun", 49];
-user[1] = 12;
-// user.push(true) // not allowed, which is as expected
-user.push(21); //allowed, TS compiler should prevent it
-const secureUser = ["arun", 49];
-// secureUser[0] = "xxxx"; not allowed
-// secureUser.push("..") not allowed
--- src/enums.ts
+++
@@ -1,9 +0,0 @@
-enum Position {
-  First = 1,
-  Second,
-  Third,
-  Fourth = 10,
-}
-
-const ajPosition = Position.First;
-const amPositoin = Position.Fourth;
